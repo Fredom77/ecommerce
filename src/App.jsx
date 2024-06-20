205264dba7fc00111ddf6675967c11a79675cefa
Fredom77 <202065@utsh.edu.mx>
2024-06-19 19:27:33 -0600

    Inicia el manejo  de variables  de  entorno

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { API_URL } from '../env';
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h1>Hola {API_URL}</h1>
     </div>
   );
 }
